acb7575487389f9eba4eaa7e15cb2d9bccbfb509
lvimage <admin@lvimage.app>
2026-06-22 23:49:09 +0800

    Add Tauri mobile entry point

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ pub mod server;
 pub mod state;
 pub mod text;
 
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_log::Builder::new().build())
