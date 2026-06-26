da1efe03f5941dccb0531bbca9f180d51f081a2f
lvimage <admin@lvimage.app>
2026-06-26 23:32:28 +0800

    style: make tools page a placeholder

--- miniprogram/wechat/pages/tools/index.js
+++ miniprogram/wechat/pages/tools/index.js
@@ -1,9 +1,4 @@
-const { tools, modeLabels } = require('../../utils/catalog')
-
-const toolCards = tools.map((tool) => ({ ...tool, modeLabel: modeLabels[tool.mode] || tool.mode }))
-
 Page({
-  data: { tools: toolCards },
   goWorkspace() {
     wx.switchTab({ url: '/pages/workspace/index' })
   },
